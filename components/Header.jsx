640a79a1102ee51b6e3c3fb8353b6a6272a78a6f
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-18 12:08:24 +0600

    Scroll Progress Hook

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
+// comps
+import Logo from './Logo';
+import ThemeToggler from './ThemeToggler';
+import Nav from './Nav';
+import MobileNav from './MobileNav';
+
+const Header = () => {
+   return (
+      <div className='container mx-auto'>
+         <div className='flex justify-between items-center'>
+            <Logo />
+            <div className='flex items-center gap-x-6'>
+               {/* nav */}
+               <Nav />
+               <ThemeToggler />
+               {/* mobile menu */}
+               <div className='xl:hidden'>
+                  <MobileNav />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Header;
