db5fa79e9dfae35e11462358d699bf580ec6a193
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-18 12:34:42 +0600

    Header Section Complete

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,23 +7,51 @@ import Logo from './Logo';
 import ThemeToggler from './ThemeToggler';
 import Nav from './Nav';
 import MobileNav from './MobileNav';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+   const [header, setHeader] = useState(false);
+   const pathname = usePathname();
+   console.log(pathname);
+
+   useEffect(() => {
+      const scrollYPos = window.addEventListener('scroll', () => {
+         window.scrollY > 50 ? setHeader(true) : setHeader(false);
+      });
+
+      // remove event
+      return () => window.removeEventListener('scroll', scrollYPos);
+   });
+
    return (
-      <div className='container mx-auto'>
-         <div className='flex justify-between items-center'>
-            <Logo />
-            <div className='flex items-center gap-x-6'>
-               {/* nav */}
-               <Nav />
-               <ThemeToggler />
-               {/* mobile menu */}
-               <div className='xl:hidden'>
-                  <MobileNav />
+      <header
+         className={`${
+            header
+               ? 'py-4 bg-white shadow-lg dark:bg-accent'
+               : 'py-6 dark:bg-transparent'
+         } sticky top-0 z-30 transition-all ${
+            pathname === '/' && 'bg-[#fef9f5]'
+         }`}
+      >
+         <div className='container mx-auto'>
+            <div className='flex justify-between items-center'>
+               <Logo />
+               <div className='flex items-center gap-x-6'>
+                  {/* nav */}
+                  <Nav
+                     containerStyles='hidden xl:flex gap-x-8 items-center'
+                     linkStyles='relative hover:text-primary transition-all'
+                     underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full'
+                  />
+                  <ThemeToggler />
+                  {/* mobile menu */}
+                  <div className='xl:hidden'>
+                     <MobileNav />
+                  </div>
                </div>
             </div>
          </div>
-      </div>
+      </header>
    );
 };
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import Link from 'next/link';
 
-const Nav = () => {
-   return <div>Nav</div>;
+import { motion } from 'framer-motion';
+
+// next hooks
+import { usePathname } from 'next/navigation';
+
+const links = [
+   { path: '/', name: 'home' },
+   { path: '/projects', name: 'my projects' },
+   { path: '/contact', name: 'contact' },
+];
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+   const path = usePathname();
+
+   return (
+      <div className={`${containerStyles}`}>
+         {links.map((link, index) => {
+            return (
+               <Link
+                  key={index}
+                  href={link.path}
+                  className={`capitalize ${linkStyles}`}
+               >
+                  {link.path === path && (
+                     <motion.span
+                        initial={{ y: '-100%' }}
+                        animate={{ y: 0 }}
+                        transition={{ type: 'tween' }}
+                        layoutId='underline'
+                        className={`${underlineStyles}`}
+                     />
+                  )}
+                  {link.name}
+               </Link>
+            );
+         })}
+      </div>
+   );
 };
 
 export default Nav;
