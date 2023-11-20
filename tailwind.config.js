a9235aaf3cc806f672e9191d02859e1b25a20d1a
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-20 19:08:47 +0600

    Contact Page left section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -87,6 +87,8 @@ module.exports = {
             dots_dark: 'url(/dots-dark.svg)',
             work_project_bg_light: 'url(/work/project-bg-light.png)',
             work_project_bg_dark: 'url(/work/project-bg-dark.png)',
+            contact_illustrations_light: 'url(/contact/illustration-light.svg)',
+            contact_illustrations_dark: 'url(/contact/illustration-dark.svg)',
          },
       },
    },
