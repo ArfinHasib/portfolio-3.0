adae804a2bf6b74fcb96162090332ea28c90a69f
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-20 16:41:20 +0600

    Building the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import Socials from './Socials';
 
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <footer className='bg-secondary dark:bg-tertiary py-12'>
+         <div className='container mx-auto'>
+            <div className='flex flex-col items-center justify-between'>
+               {/* socials */}
+               <Socials
+                  containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4'
+                  iconStyles='text-primary dark:text-white/40 text-[20px] hover:text-white dark:hover:text-primary transition-all'
+               />
+               {/* copyright */}
+               <div className='text-muted-foreground'>
+                  Copyright &copy; Arfin Hasib. All rights reserved.
+               </div>
+            </div>
+         </div>
+      </footer>
+   );
 };
 
 export default Footer;
